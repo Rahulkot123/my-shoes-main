b17169cdeae62fb8a924f8c132c38f4cc038cafd
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-13 23:12:51 +0100

    update kids done

--- src/Pages/Kids.js
+++ src/Pages/Kids.js
@@ -12,17 +12,17 @@ export default function Kids() {
     <Filters/>
 
     <div className="Content">
-        <img src={ require('../Photos/Offers1.webp')} alt='offers product 1'></img>
-        <img src={ require('../Photos/offers2.webp')} alt='offers product 2 '></img>
-        <img src={ require('../Photos/offers3.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers4.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids1.webp')} alt='offers product 1'></img>
+        <img src={ require('../Photos/kids2.webp')} alt='offers product 2 '></img>
+        <img src={ require('../Photos/kids3.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids4.webp')} alt='offers product 3 '></img>
     </div>
 
     <div className="Content">
-        <img src={ require('../Photos/offers5.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers6.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers7.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers8.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids5.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids6.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids7.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/kids8.webp')} alt='offers product 3 '></img>
    </div>
 
 </div>
--- src/Pages/Women.js
+++ src/Pages/Women.js
@@ -14,17 +14,17 @@ export default function Women() {
 
 
     <div className="Content">
-        <img src={ require('../Photos/Offers1.webp')} alt='offers product 1'></img>
-        <img src={ require('../Photos/offers2.webp')} alt='offers product 2 '></img>
-        <img src={ require('../Photos/offers3.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers4.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/women1.webp')} alt='offers product 1'></img>
+        <img src={ require('../Photos/women2.webp')} alt='offers product 2 '></img>
+        <img src={ require('../Photos/woman3.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/woman4.webp')} alt='offers product 3 '></img>
     </div>
 
     <div className="Content">
-        <img src={ require('../Photos/offers5.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers6.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers7.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers8.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/woman5.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/Women6.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/woman7.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/woman8.webp')} alt='offers product 3 '></img>
    </div>
         </div>
 
