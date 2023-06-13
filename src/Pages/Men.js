12efb736240ae19d356e24338c21fc8c2316095c
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-13 22:23:02 +0100

    update men  page done

--- src/Pages/Men.js
+++ src/Pages/Men.js
@@ -16,17 +16,17 @@ export default function Men() {
     <Filters/>
 
     <div className="Content">
+        <img src={ require('../Photos/men1.webp')} alt='offers product 2 '></img>
         <img src={ require('../Photos/Offers1.webp')} alt='offers product 1'></img>
-        <img src={ require('../Photos/offers2.webp')} alt='offers product 2 '></img>
         <img src={ require('../Photos/offers3.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers4.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/men4.jfif')} alt='offers product 3 '></img>
     </div>
 
     <div className="Content">
-        <img src={ require('../Photos/offers5.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers6.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers7.webp')} alt='offers product 3 '></img>
-        <img src={ require('../Photos/offers8.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/men5.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/men6.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/men7.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/men8.webp')} alt='offers product 3 '></img>
    </div>
     
     
