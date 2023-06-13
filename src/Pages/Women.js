a91d82bc32a939132ab4713ab0ca7709ceb09eb5
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-13 16:50:42 +0100

    items added

--- src/Pages/Women.js
+++ src/Pages/Women.js
@@ -12,6 +12,20 @@ export default function Women() {
     <Sortby/>
     <Filters/>
 
+
+    <div className="Content">
+        <img src={ require('../Photos/Offers1.webp')} alt='offers product 1'></img>
+        <img src={ require('../Photos/offers2.webp')} alt='offers product 2 '></img>
+        <img src={ require('../Photos/offers3.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/offers4.webp')} alt='offers product 3 '></img>
+    </div>
+
+    <div className="Content">
+        <img src={ require('../Photos/offers5.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/offers6.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/offers7.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/offers8.webp')} alt='offers product 3 '></img>
+   </div>
         </div>
 
 
