f47027b7e75aeff0e5c838ce5e4279189323e4c0
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-13 23:25:09 +0100

    accessories done

--- src/Pages/Accessories.js
+++ src/Pages/Accessories.js
@@ -9,6 +9,22 @@ export default function Accessories (){
     <div className='bannar'></div>
     <Sortby/>
     <Filters/>
+   
+    <div className="Content">
+        <img src={ require('../Photos/ac1.webp')} alt='offers product 2 '></img>
+        <img src={ require('../Photos/ac2.webp')} alt='offers product 1'></img>
+        <img src={ require('../Photos/ac3.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/ac4.webp')} alt='offers product 3 '></img>
+    </div>
+
+    <div className="Content">
+        <img src={ require('../Photos/ac5.webp')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/ac6.jpg')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/ac7.jpg')} alt='offers product 3 '></img>
+        <img src={ require('../Photos/ac8.jpg')} alt='offers product 3 '></img>
+   </div>
+    
+
 
 </div>
     )
