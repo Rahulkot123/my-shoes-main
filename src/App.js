9b1207960c5857386de10ff5603601b4082a457e
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-14 11:52:53 +0100

    signup page start

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Offers from './Pages/Offers'
 import Men from './Pages/Men'
 import Women from './Pages/Women';
 import Kids from './Pages/Kids'
+import Contact from './Pages/Contact';
 import Accessories from './Pages/Accessories';
+import Login from './Pages/Login';
+import Signup from './Pages/SIgnUp';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -20,10 +23,10 @@ function App() {
         <Route path='/Women' element={<Women/>}/>
         <Route path='/Kids' element={<Kids/>}/>
         <Route path='/Accessories' element={<Accessories/>}/>
-        <Route/>
-        <Route/>
-        <Route/>
-      </Routes>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Signup' element= {<SignUp/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+      </Routes >
     </div>
 
     
