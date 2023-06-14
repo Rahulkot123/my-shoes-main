ab197673b4eb374c90cfad09e4a4847fd5e21f40
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-14 13:55:49 +0100

    login start

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,49 @@
 import React from "react";
+import './login.css'
 
 export default function Login(){
     return(
         <div>
+
+            <div className="benfits">
+                <h2>With an Account You Can:</h2>
+                <p>Get Latest news On MyShoes</p>
+                <p>Access Saved Items</p>
+                <p>Free Click & Collect </p>
+                <p>Manage and Track Items</p>
+
+            </div>
+
+
+
+
+            <div className="Container">
+                <label for= "Email"><b>Email*</b></label>
+                <input type="text" placeholder="Enter Email "></input>
+
+                <label for= "Password"><b>Password*</b></label>
+                <input type="text" placeholder="Enter Password "></input>
+
+                <button type="Submit"> Login</button>
+ 
+                    <span className="psw">  <a href="email">Forgot Password</a></span>
+                     
+                      <span >  <a href="signup">Create An Account</a></span>           
+            </div>
+                
+
+            <footer className='homefooter'>
             
+            <p>phone number</p>
+            <p>Address</p>
+            <p>harrowstore@myshoes.com</p>
+            
+        
+        </footer>
+
         </div>
+
+
+
     )
 }
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,16 +1,12 @@
 import React from "react";
-
+import './Men.css';
+import './Offers.css';
 
 
 export default function Signup(){
     return(
         <div>
-        <div className="container">
-            <h1>Register to MyShoes Account</h1>
-            <p>Please enter details here</p>
-
-
-        </div>
+            
         </div>
     )
 }
