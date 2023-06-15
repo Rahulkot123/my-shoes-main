de83b51995127cfca36970b1b988ffb1e89e63cf
Rahulkot123 <119442952+Rahulkot123@users.noreply.github.com>
2023-06-15 23:19:05 +0100

    deadline

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar(){
     const Navigate = useNavigate()
+    
 
     return <div>
         <header className='topnav'>
