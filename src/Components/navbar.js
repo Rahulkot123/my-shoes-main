d28163896af51f78f6590865855ce14eeaca0dae
Rahulkot123 <40182403@hcuc.ac.uk>
2023-06-15 10:21:57 +0100

    ...

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -7,14 +7,14 @@ export default function Navbar(){
         <header className='topnav'>
             <img src={logo} alt='logo'/>
             <a href='/'>Home</a>
-            <a href='Offers'>Offers</a>
-            <a href='Men'>Men</a>
-            <a href='Women'>Women</a>
-            <a href='kids'>Kids</a>
-            <a href='Accessories'>Accessories</a>
-            <a href='login'>Login</a>
-            <a href='Signup'>Signup</a>
-            <a href='Contact'>Contact</a>
+            <a href='/Offers'>Offers</a>
+            <a href='/Men'>Men</a>
+            <a href='/Women'>Women</a>
+            <a href='/kids'>Kids</a>
+            <a href='/Accessories'>Accessories</a>
+            <a href='/login'>Login</a>
+            <a href='/Signup'>Signup</a>
+            <a href='/Contact'>Contact</a>
             <img src={ require('../Photos/login.png')}alt='login button'/>
             <img src={ require('../Photos/phone.png')}alt='contact page button'/>
             <img src={ require('../Photos/basket.png')} alt='basket button'/>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,6 +4,17 @@ import './contact.css'
 export default function Contact(){
     return(
         <div>
+
+        <div classname="Feebackform">
+        
+        <h1>heelo</h1>
+
+        <div className="Email">
+            <label for="Email"><b>Email</b></label>
+            <input type="Email" placeholder="Enter Email" name="email" required/>
+        </div> 
+
+        </div>
             
         </div>
     )
